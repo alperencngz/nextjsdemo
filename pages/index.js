a9cf7f11f13f5adeae23fbff57b3cb1dcaf5d2bc
alperencngz <cengiz.ozturk@ozu.edu.tr>
2023-07-07 10:04:03 +0300

    Fixed some bugs and created index.js with dummy data rendering

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "First Street 5",
+    description: "This is our first meetup",
+  },
+
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Second Street 6",
+    description: "This is our second meetup",
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;
