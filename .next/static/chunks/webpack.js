97ef289236f354a95727457847a180d93fbb656d
alperencngz <cengiz.ozturk@ozu.edu.tr>
2023-07-13 09:30:29 +0300

    Added our general layout to app.js
    _app.js is like the root folder of our project

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "843f61f31b724aa0"; }
+/******/ 		__webpack_require__.h = function() { return "06d4e34d4ee90eeb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.82aa72f76296b05c.hot-update.js
+++ .next/static/webpack/webpack.82aa72f76296b05c.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "5d3b94b0e377aa31"; }
+/******/ 	__webpack_require__.h = function() { return "0ebc6add34e34e44"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
